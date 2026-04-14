8055aae8d80717691c9374b1d7d45835a67b62ca
Mohammed Yousuf <191001678+MohammedYousufCode@users.noreply.github.com>
2026-04-14 12:24:34 +0530

    chore: ignore raw data folder

---
+++ sql/data_validation.sql
@@ -0,0 +1,9 @@
+SELECT COUNT(*) AS total_rows from hr_employee;
+SELECT COUNT(*) AS null_employee_ids FROM hr_employee WHERE EmployeeNumber IS NULL;
+SELECT Attrition,
+COUNT(*) AS total,
+ROUND(COUNT(*)*100.0/SUM(COUNT(*)) OVER(),2) AS percentage 
+FROM hr_employee
+GROUP BY Attrition 
+ORDER BY total DESC;
+
